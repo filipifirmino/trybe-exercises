febc00bf35166c12acd9eceac7d501f0d6cb99e4
Filipi Firmino <contatoic3web@gmail.com>
2020-08-28 19:51:23 -0300

    exercicios de array

---
+++ bloco_4/4_2/exer_4array.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
+for (let i = 0;i< numbers.length;i++){
+    total += numbers[i];
+}
+let media = total / numbers.length;
+
+if(media>20){
+    console.log("Valor maior que 20")
+}else{
+    console.log("Valor menor que 20")
+}
+console.log(media)
