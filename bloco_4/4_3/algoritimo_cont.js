6cc5451028b3477980961fc18ef6bc046ba13e37
Filipi Firmino <contatoic3web@gmail.com>
2020-08-31 16:49:46 -0300

    Resoluçõa do algritimo do conteudo

---
+++ bloco_4/4_3/algoritimo_cont.js
@@ -0,0 +1,15 @@
+let fruits = [3, 4, 10, 1, 12];
+let result = 0;
+
+for(let i=0; i<fruits.length; i+=1){
+     result += fruits[i];
+     
+}
+
+if(result>15){
+    console.log(`O valor é ${result}`)
+}else if(result < 15){
+    console.log('Resultado é menor que 16')
+}
+
+
