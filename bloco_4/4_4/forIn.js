2e8425379ee467dcd844b3e12057416876cc6145
Filipi Firmino <contatoic3web@gmail.com>
2020-09-01 20:24:21 -0300

    Exercices of Js function and object

---
+++ bloco_4/4_4/forIn.js
@@ -0,0 +1,16 @@
+//for in
+
+const peaple = {
+    tipe: 'Human',
+    age : '100'
+
+}
+
+for (let chave in peaple){
+    console.log(chave, pessoa['nome'])
+}
+
+const cores = ['azul', 'verde','preto']
+
+
+// Este tipo de for é usado para percorrer objetos.
