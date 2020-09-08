c55cd74042d5b3ca27d8c2a339170e10628e5d7a
Filipi Firmino <contatoic3web@gmail.com>
2020-09-08 18:37:56 -0300

    Questao 2 do exercicio resolvido

--- bloco_5/5.2/asset/js/main.js
+++ bloco_5/5.2/asset/js/main.js
@@ -1,6 +1,13 @@
 //Criando tag h1
 let title = document.createElement("h1");
 //Adcionando texto a tag h1
-title.innerText = "Exercício 5.2 - JavaScript DOM"
+title.innerText = "Exercício 5.2 - JavaScript DOM";
 //Inserido tag ao dom
-document.body.appendChild(title)
+document.body.appendChild(title);
+
+//Criando elemento div
+let content = document.createElement('div');
+//Atribuindo uma classe a div
+content.className = "main-content";
+//Inserindo a tag como filha do body
+document.body.appendChild(content);
