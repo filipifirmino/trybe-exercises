b6b32b01a1cf201d17771ce79b5733601aa380f7
Filipi Firmino <contatoic3web@gmail.com>
2020-09-10 20:46:46 -0300

    Script js para interação semi pronto

--- bloco_5/5.4/asset/js/main.js
+++ bloco_5/5.4/asset/js/main.js
@@ -12,6 +12,8 @@ function tema() {
         let title = document.querySelector('.title');
         let text = document.querySelector('.text');
         let painel = document.querySelector('.painel');
+        let image = document.querySelector('.imgLogo');
+        image.src = "/asset/image/logo_with.png";
         painel.style.color="#fff";
         title.style.color = "#fff";
         text.style.color = "#fff";
@@ -22,10 +24,33 @@ function tema() {
         let title = document.querySelector('.title');
         let text = document.querySelector('.text');
         let painel = document.querySelector('.painel');
+        let image = document.querySelector('.imgLogo');
+        image.src = "/asset/image/logo_black.png";
         painel.style.color="#000";
         title.style.color = "#000";
         text.style.color = "#000";
         
     }
+
    
 }
+
+let selectFont = document.querySelector('#option');
+let selectSize = document.querySelector('#fontOption');
+let selectSpace = document.querySelector('#spaceOption');
+
+selectFont.addEventListener('select', option);
+selectSize.addEventListener('select', option);
+selectSpace.addEventListener('select', option);
+
+function option(){
+    let font = document.querySelector('#painel');
+    let sizeFont = document.querySelector('#fontOption');
+    let space = document.querySelector('#spaceOption');
+
+    font.style.fontFamily = selectFont.value;
+    sizeFont.style.fontSize = selectSize.value;
+   // space.style.fontFamily = selectFont.value;
+    //Aplicar no texto
+}
+//web Storage 
