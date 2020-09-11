9264e19b83a1ccf9ba457f5ecf9ed553daf93874
Filipi Firmino <contatoic3web@gmail.com>
2020-09-11 11:56:47 -0300

    Cat moonwalker ok

--- bloco_5/5.3/catWalk.js
+++ bloco_5/5.3/catWalk.js
@@ -1,19 +1,22 @@
 const image = document.querySelector('#cat');
 image.style.marginLeft = "0px";
 let windowSize = window.innerWidth;
+let walkGoing = true;
 
-setInterval(walk, 50);
 function walk(){
-    image.style.marginLeft = parseInt(image.style.marginLeft) + 10 + "px";
-    if(parseInt(image.style.marginLeft) >= windowSize){
-       image.style.marginLeft = '0px';
-       /*
-       while(image.style.marginLeft !== "0px"){
-        image.style.marginLeft = parseInt(image.style.marginLeft) - 10 + "px";
-       }*/
-       //Andar para trás
-    }
-    console.log(image.style.marginLeft);
-
     
+if(parseInt(image.style.marginLeft) >= windowSize){
+// image.style.marginLeft = '0px';
+walkGoing = false;
+}else if(parseInt(image.style.marginLeft) == 0){
+    walkGoing = true;
 }
+if(walkGoing){
+image.style.marginLeft = parseInt(image.style.marginLeft) + 10 + "px";
+}else{
+image.style.marginLeft = parseInt(image.style.marginLeft) - 10 + "px";
+//Andar para trás
+}
+   
+}
+setInterval(walk, 50);
