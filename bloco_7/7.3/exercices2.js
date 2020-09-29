95b186f1a6e1c82b11348aea3d0c2faddc77cafd
Filipi Firmino <contatoic3web@gmail.com>
2020-09-29 19:20:22 -0300

    Exercices 2

---
+++ bloco_7/7.3/exercices2.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+
+// Step1
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+
+// Step2 
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+// Step 3
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4], 'Sofreu alteração');
+
+// Step 4
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
