007046953f40073f36e3332563e80414cdd0f466
Filipi Firmino <contatoic3web@gmail.com>
2020-09-28 22:48:21 -0300

    exercices 7.1

---
+++ bloco_7/7.1/exerc4.js
@@ -0,0 +1,14 @@
+const longWord = (phrase) => {
+   let words = phrase.split(' ');
+   for(let index = 0 ; index < words.length; index += 1) {
+       for(let indice = 0; indice < words.length; indice += 1) {
+           if(words[index].length > words[indice].length){
+               let result =  words[index]; 
+               let bigWord = result;
+           }
+           console.log(result);
+       }
+   }
+}
+
+longWord('Meu nome não é jonyy');
